f4c209f5c4b126bfea18fd196c7925ef783de18e
Svetlana Fedorenko <svetlana493@ukr.net>
2022-07-31 11:25:39 +0300

    дз

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,69 @@
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { Notification } from './Notification/Notification';
+import { Container, MainTitle, SubTitle } from './App.styled';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (name, number) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    if (
+      contacts.some(
+        contactItem =>
+          contactItem.name.toLowerCase() === contact.name.toLowerCase()
+      )
+    ) {
+      return alert(`${contact.name} is already in contacts`);
+    }
+    setContacts(prevState => [contact, ...prevState]);
+  };
+
+  const deleteContact = deleteId => {
+    setContacts(contacts.filter(({ id }) => id !== deleteId));
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const filterContacts = () => {
+    const normalizeFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizeFilter)
+    );
+  };
+
+  const visibleContacts = filterContacts();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <MainTitle>Phonebook</MainTitle>
+      <ContactForm addContact={addContact} />
+      <SubTitle>Contacts</SubTitle>
+      {contacts.length > 1 && (
+        <Filter filter={filter} changeFilter={changeFilter} />
+      )}
+      {contacts.length ? (
+        <ContactList contacts={visibleContacts} deleteContact={deleteContact} />
+      ) : (
+        <Notification message="List is empty" />
+      )}
+    </Container>
   );
 };
